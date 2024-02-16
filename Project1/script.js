8aed2b69650c9964e3354f48abaa72cfd5e1f487
Noam Benkler <nbenkler@sift.net>
2024-02-15 22:02:36 -0500

    fixed up a few things

--- Project1/script.js
+++ Project1/script.js
@@ -20,3 +20,45 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+// Schedule download
+function downloadTSV(data, filename) {
+    let blob = new Blob([data], { type: 'text/tab-separated-values' });
+    let a = document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename + '.tsv';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+document.getElementById('download-btn').addEventListener('click', function () {
+    let tourRows = document.querySelectorAll('#tour-table tr');
+    let daytimeRows = document.querySelectorAll('#daytime-table tr');
+    let tourTSV = 'Date\tCity\tVenue\tTime\n';
+    let daytimeTSV = 'Day\tSunday\tMonday\tTuesday\tWednesday\tThursday\tFriday\tSaturday\n';
+
+    tourRows.forEach(function(row) {
+        let rowData = [];
+        row.querySelectorAll('td').forEach(function(cell) {
+            rowData.push(cell.innerText.trim());
+        });
+        tourTSV += rowData.join('\t') + '\n';
+    });
+
+    daytimeRows.forEach(function(row) {
+        let rowData = [];
+        row.querySelectorAll('td').forEach(function(cell) {
+            rowData.push(cell.innerText.trim());
+        });
+        daytimeTSV += rowData.join('\t') + '\n';
+    });
+
+    downloadTSV(tourTSV, 'tour_schedule');
+    downloadTSV(daytimeTSV, 'daytime_tred');
+});
+
+function displayMessage() {
+    var messageContainer = document.getElementById("messageContainer");
+    messageContainer.textContent = "Thanks for the message, we will respond in short order. Look out for our pigeon!";
+}
